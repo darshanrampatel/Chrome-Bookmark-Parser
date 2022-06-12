826e1b7a7bc7216fa54f015a1a9439ea4999dea1
Darshan <darshanrampatel@users.noreply.github.com>
2022-06-12 23:06:39 +0100

    Changes

--- ChromeBookmarkParser/Program.cs
+++ ChromeBookmarkParser/Program.cs
@@ -13,73 +13,95 @@ namespace ChromeBookmarkParser
         static void Main()
         {
             var downloadFolderPath = new KnownFolder(KnownFolderType.Downloads).Path;
-            var bookmarks = Directory.EnumerateFiles(downloadFolderPath, "bookmarks_*.html");
-            var bookmarksCounts = bookmarks.Count();
-            if (bookmarksCounts != 1)
+            var bookmarks = Directory.EnumerateFiles(downloadFolderPath, "bookmarks_*.html").OrderByDescending(f => f).ToList();
+            var bookmarkFile = bookmarks.FirstOrDefault();
+            if (bookmarks.Count != 1)
             {
-                Console.WriteLine($"ERROR: {bookmarks.Count()} possible files found");
-                Console.WriteLine($"Please ensure there is {(bookmarksCounts == 0 ? "" : "only ")}one possible file and try again");
+                Console.WriteLine($"{bookmarks.Count} possible files found");
+                Console.WriteLine($"Using latest file based on name: {bookmarkFile}");
             }
-            else
+            var regex = new Regex(@"(\d{4})");
+            var filmsList = new List<string>();
+            var booksList = new List<string>();
+            var doc = new HtmlDocument();
+            doc.Load(bookmarkFile);
+            var sn = doc.DocumentNode.LastChild.PreviousSibling;
+            var t = sn.Descendants();
+            var seenFilms = false;
+            var seenBooks = false;
+            foreach (var i in t)
             {
-                var regex = new Regex(@"(\d{4})");
-                var filmsList = new List<string>();
-                var doc = new HtmlDocument();
-                doc.Load(bookmarks.First());
-                var sn = doc.DocumentNode.LastChild.PreviousSibling;
-                var t = sn.Descendants();
-                foreach (var i in t)
+                if (i.Name == "dt")
                 {
-                    if (i.Name == "dt")
+                    var childNodes = i.ChildNodes;
+                    var h3 = childNodes.FindFirst("H3");
+                    if (h3?.InnerText == "Bookmarks bar")
                     {
-                        var childNodes = i.ChildNodes;
-                        var h3 = childNodes.FindFirst("H3");
-                        if (h3.InnerText == "Bookmarks bar")
+                        var cnFilms = h3.NextSibling.NextSibling.LastChild.LastChild.LastChild.LastChild.LastChild.LastChild.ChildNodes;
+                        if (!seenFilms && cnFilms.First().InnerText == "Films")
                         {
-                            var cn = h3.NextSibling.NextSibling.LastChild.LastChild.LastChild.LastChild.LastChild.LastChild.ChildNodes;
-                            if (cn.First().InnerText == "Films")
+                            var f = cnFilms.FindFirst("p").LastChild.LastChild.ChildNodes;
+                            foreach (var film in f)
                             {
-                                var f = cn.FindFirst("p").LastChild.LastChild.ChildNodes;
-                                foreach (var film in f)
+                                if (film.Name == "dt")
                                 {
-                                    if (film.Name == "dt")
-                                    {
-                                        filmsList.Add(System.Net.WebUtility.HtmlDecode(film.FirstChild.InnerText));
-                                    }
+                                    filmsList.Add(System.Net.WebUtility.HtmlDecode(film.FirstChild.InnerText));
                                 }
-                                break;
+                            }
+                            seenFilms = true;
+                        }
+                    }
+
+                    var cnBooks = h3?.NextSibling.NextSibling.LastChild.LastChild.ChildNodes;
+                    if (!seenBooks && cnBooks.First().InnerText == "Books")
+                    {
+                        var b = cnBooks.FindFirst("p").LastChild.LastChild.ChildNodes;
+                        foreach (var book in b)
+                        {
+                            if (book.Name == "dt")
+                            {
+                                booksList.Add(System.Net.WebUtility.HtmlDecode(book.FirstChild.InnerText));
                             }
                         }
-                    }
-
-                }
-
-                Console.WriteLine($"Reading {filmsList.Count} films");
-
-                var years = new Dictionary<int, List<string>>();
-                foreach (var line in filmsList)
-                {
-                    var dateFound = regex.Match(line.Trim());
-                    if (dateFound.Success && int.TryParse(dateFound.Value, out int year))
-                    {
-                        if (!years.ContainsKey(year))
-                        {
-                            years.Add(year, new List<string>());
-                        }
-                        years[year].Add(line.Replace($"({dateFound})", ""));
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Ignoring: {line}");
+                        seenBooks = true;
                     }
                 }
-                foreach (var year in years.OrderBy(y => y.Key))
+
+            }
+
+            Console.WriteLine($"Reading {filmsList.Count} films");
+
+            var years = new Dictionary<int, List<string>>();
+            foreach (var line in filmsList)
+            {
+                var dateFound = regex.Match(line.Trim());
+                if (dateFound.Success && int.TryParse(dateFound.Value, out int year))
                 {
-                    Console.WriteLine();
-                    Console.WriteLine($"---{year.Key}---");
-                    Console.WriteLine($"{string.Join(Environment.NewLine, year.Value.OrderBy(f => f).Select(f => f))}");
+                    if (!years.ContainsKey(year))
+                    {
+                        years.Add(year, new List<string>());
+                    }
+                    years[year].Add(line.Replace($"({dateFound})", ""));
+                }
+                else
+                {
+                    Console.WriteLine($"Ignoring: {line}");
                 }
             }
+            foreach (var year in years.OrderBy(y => y.Key))
+            {
+                Console.WriteLine();
+                Console.WriteLine($"---{year.Key}---");
+                Console.WriteLine($"{string.Join(Environment.NewLine, year.Value.OrderBy(f => f).Select(f => f))}");
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine($"Reading {booksList.Count} books");
+            //foreach (var book in booksList.OrderBy(b => b))
+            //{
+            //    Console.WriteLine(book);
+            //}
             Console.ReadLine();
         }
     }
